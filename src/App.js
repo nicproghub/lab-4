5a31999dd06bd6db3ac76137630341e3fcc06750
Nic <21686317@student.latrobe.edu.au>
2024-08-16 14:24:00 +1000

    add checkbox, use  props and use map to update props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import List from './components/List';
 import './App.css';
 
+const v_tasks =[
+    {des: 'Learn React', comp:true},
+    {des: 'Learn JSX use Map', comp: false},
+    {des: 'Build a React App', comp: false}
+];
+
 function App() {
     return (
         <div className='page'>     
-            <List heading='My Tasks'/>
+            <List heading='My Tasks' tasks={v_tasks}/>
         </div>
     );
 }
--- src/components/List.js
+++ src/components/List.js
@@ -1,11 +1,20 @@
+function Task(props) {
+	return (
+		<li>{props.description}<input type='checkbox' checked={props.completed} readOnly/> </li>
+	);
+}
+
 function List(props) {
 	return (
 		<div>
 			<h1>{ props.heading }</h1>
 			<ul>
-				<li>Learn React</li>
-				<li>Learn JSX</li>
-				<li>Build a React App</li>
+				{/* {tasks} */}
+				{props.tasks.map(x=>
+					<Task description={x.des}
+					completed={x.comp}
+					/>
+				)}
 			</ul>
 		</div>
 	);
